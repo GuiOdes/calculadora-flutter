e7c8ce92daa9fe80818c667555ebf7b3b7ac8d15
zryanf <80858407+zryanf@users.noreply.github.com>
2025-01-23 08:50:59 -0300

    erro de compilação

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:app/ThemeData.dart';
 import 'package:app/pages/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
 import 'components/MyAppBar.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  const keyApplicationId = 'QdZED23aJMjxCSyFLgNucNmsq4lCrhJTBOFfY10e';
+  const keyClientKey = 'z3nz8OnTPVqECG5gXQuWRa8xUiOEhJZYOGB8XoGQ';
+  const keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(
+    keyApplicationId,
+    keyParseServerUrl,
+    clientKey: keyClientKey,
+    autoSendSessionId: true,
+    debug: true,
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
